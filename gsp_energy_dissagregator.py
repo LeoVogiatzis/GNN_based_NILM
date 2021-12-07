6719267f36fc2a96518d11c0bc3abf4f42c6c2cb
LeoVogiatzis <leonvogiatzis@gmail.com>
2021-12-08 00:30:25 +0200

    Add features and adjacency as edge index.

--- gsp_energy_dissagregator.py
+++ gsp_energy_dissagregator.py
@@ -9,6 +9,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import torch
+import torch_geometric
+from torch_geometric.data import Dataset, Data
+from tqdm import tqdm
+from torch_geometric.utils import from_scipy_sparse_matrix
+
+print(f"Torch version: {torch.__version__}")
+print(f"Cuda available: {torch.cuda.is_available()}")
+print(f"Torch geometric version: {torch_geometric.__version__}")
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch_geometric.transforms import RandomLinkSplit, RandomNodeSplit
+import torch
+from torch.nn import Linear
+from torch_geometric.nn import GCNConv
+import torch.nn.functional as F
 
 
 def graph_creation(data_vec, sigma,
@@ -17,7 +34,7 @@ def graph_creation(data_vec, sigma,
 
     for k in range(0, int(np.floor(len(data_vec) / window))):
         data_aggr.append(np.mean(data_vec[k * window:((k + 1) * window)]))
-    
+
     if (len(data_vec) % window > 0):
         data_aggr.append(np.mean(data_vec[int(np.floor(len(data_vec) / window)) * window:]))
     delta_p = [np.round(data_aggr[i + 1] - data_aggr[i], 2) for i in range(0, len(data_aggr) - 1)]
@@ -25,13 +42,10 @@ def graph_creation(data_vec, sigma,
     for i in range(0, Am.shape[0]):
         for j in range(0, Am.shape[1]):
             Am[i, j] = math.exp(-((delta_p[i] - delta_p[j]) / sigma) ** 2)
-    return Am
+    return Am, delta_p
 
 
 def main():
-    # G = nx.read_gpickle('graphs/microwave_3.gpickle')
-    house5 = pd.read_csv('data/house5.csv')
-
     df = pd.read_csv('data/house5.csv')  # read demo file with aggregated active power
 
     # Please read the paper to undertand following parameters. Note initial values of these parameters depends on the appliances used and the frequency of usage.
@@ -50,11 +64,16 @@ def main():
     main_val = df['dishwaser_20'].values  # get only readings
     # main_ind = df.index  # get only timestamp
     data_vec = main_val
-    # signature_database = "signature_database_labelled.csv"  # the signatures were extracted of power analysis from April 28th to 30th
     threshold = 2000  # threshold of DTW algorithm used for appliance power signature matching
 
-    Am = graph_creation(data_vec, sigma, window=77)
-    x = 1
+    Am, delta_p = graph_creation(data_vec, sigma, window=77)
+    edge_indices = torch.tensor(Am)
+    drift = torch.tensor(delta_p, dtype=torch.float)
+    labels = [0 if i == 0.0 else i for i in drift]
+    labels = [1 if (i > 0 and i != 0) else -1 for i in drift]
+    labels = torch.tensor(labels, dtype=torch.int64)
+    # edge_indices = edge_indices.t().to(torch.long).view(2, -1)
+    dataset = Data(x=drift, edge_index=edge_indices, y=labels)
 
 
 if __name__ == '__main__':
